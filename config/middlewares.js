02c673a38e48274ff5d023f2cfeeb235ec3bfa53
Yevhenii Alieksandrov <evhenii.aleksandrov@otakoyi.com>
2024-04-19 18:12:45 +0300

    feat: cloudinary integration

--- config/middlewares.js
+++ config/middlewares.js
@@ -13,16 +13,20 @@ module.exports = [
     config: {
       contentSecurityPolicy: {
         directives: {
+          upgradeInsecureRequests: null,
+          'connect-src': ["'self'", 'https:', 'http:'],
           "script-src": ["'self'", "editor.unlayer.com"],
           "script-src-elem": ["'self'", "editor.unlayer.com"],
           "frame-src": ["'self'", "editor.unlayer.com"],
+          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
           "img-src": [
             "'self'",
             "data:",
-            "market-assets.strapi.io",
-            "cdn.jsdelivr.net",
             "strapi.io",
+            "cdn.jsdelivr.net",
             "s3.amazonaws.com",
+            'res.cloudinary.com',
+            "market-assets.strapi.io",
           ],
         },
       },
