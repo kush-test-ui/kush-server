19406c866ad907bf3923c4d68d6418acb02d5356
Liubov Smahliuk <liubov.smahliuk-contractor@procore.com>
2024-11-01 13:13:01 +0200

    hardcode host

--- config/database.js
+++ config/database.js
@@ -50,7 +50,7 @@ module.exports = ({ env }) => {
     postgres: {
       connection: {
         connectionString: env('DATABASE_URL'),
-        host: env('DATABASE_HOST', 'localhost'),
+        host: env('DATABASE_HOST', 'kush-dev.cpoeg2ikgxrl.eu-central-1.rds.amazonaws.com'),
         port: env.int('DATABASE_PORT', 5432),
         database: env('DATABASE_NAME', 'strapi'),
         user: env('DATABASE_USERNAME', 'strapi'),
