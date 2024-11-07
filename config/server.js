f5754e9cb81659bb9f6e1c6d77946090f8a95122
evhenii.alieksnadrov <yosorit@gmail.com>
2024-11-07 13:59:04 +0200

    fix

--- config/server.js
+++ config/server.js
@@ -1,13 +1,11 @@
 module.exports = ({ env }) => {
-  console.log(env('PUBLIC_URL'));
-
   return {
     host: env('HOST', '0.0.0.0'),
     port: env.int('PORT', 1337),
     app: {
       keys: env.array('APP_KEYS'),
     },
-    url: env('PUBLIC_URL'),
+    url: env('PUBLIC_URL', 'https://api.kush.jewelry'),
     webhooks: {
       populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
     },
