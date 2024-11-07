3cc0e774fa6d1f327c3242a6f69377cb4e681b7f
evhenii.alieksnadrov <yosorit@gmail.com>
2024-11-07 13:44:32 +0200

    fix: .env variables

--- config/server.js
+++ config/server.js
@@ -1,11 +1,15 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
-  url: env('PUBLIC_URL', 'https://dev.kush.jewelry'),
-  webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
-  },
-});
+module.exports = ({ env }) => {
+  console.log(env('PUBLIC_URL'));
+
+  return {
+    host: env('HOST', '0.0.0.0'),
+    port: env.int('PORT', 1337),
+    app: {
+      keys: env.array('APP_KEYS'),
+    },
+    url: env('PUBLIC_URL'),
+    webhooks: {
+      populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    },
+  };
+};
