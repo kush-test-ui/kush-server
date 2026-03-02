cd21c2fc84ab521e54b823c5a66bbee70518d262
yevhenii.alieksandrov <yosorit@gmail.com>
2026-03-03 00:04:12 +0200

    fi

--- src/index.js
+++ src/index.js
@@ -15,5 +15,25 @@ module.exports = {
    * This gives you an opportunity to set up your data model,
    * run jobs, or perform some special logic.
    */
-  async bootstrap(/* { strapi } */) {},
+  async bootstrap({ strapi }) {
+    const requiredContentTypes = [
+      'api::home.home',
+      'api::global.global',
+      'api::about-us-page.about-us-page',
+      'api::products-page.products-page',
+    ];
+
+    const loadedApiContentTypes = Object.keys(strapi.contentTypes || {}).filter((uid) =>
+      uid.startsWith('api::')
+    );
+
+    const missingRequired = requiredContentTypes.filter((uid) => !loadedApiContentTypes.includes(uid));
+
+    strapi.log.info(`[diag] Loaded API content-types count: ${loadedApiContentTypes.length}`);
+    strapi.log.info(`[diag] Required API content-types present: ${missingRequired.length === 0}`);
+
+    if (missingRequired.length > 0) {
+      strapi.log.warn(`[diag] Missing required content-types: ${missingRequired.join(', ')}`);
+    }
+  },
 };
